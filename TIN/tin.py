f689b7ff449d740e55d391fac3bda214c4e43567
Meshach ishaya <meshachm126@gmail.com>
2018-12-04 23:35:45 +0100

    latest

--- TIN/tin.py
+++ TIN/tin.py
@@ -12,12 +12,12 @@ listOfFiles = os.listdir('./xmls')
 #print(listOfFiles)
 
 for entry in listOfFiles:
-    for tin in range(len(tins)):
-      pattern = str(tins[tin]) + ".xml"
-      if fnmatch.fnmatch(entry, pattern):
-          #the following logic moves the sorted files from the xmls folder to a new directory called sorted
-          os.rename('./xmls/' + pattern, './sorted/' + pattern)
-          tin_count = tin_count + 1
+  for tin in range(len(tins)):
+    pattern = str(tins[tin]) + ".xml"
+    if fnmatch.fnmatch(entry, pattern):
+    #the following logic moves the sorted files from the xmls folder to a new directory called sorted
+        os.rename('./xmls/' + pattern, './sorted/' + pattern)
+        tin_count = tin_count + 1
 
-print(tin_count , " number of xml files in xmls directory where found corresponding to the excel file TIN column and moved to the sorted folder ")
+print(tin_count, " number of xml files in xmls directory where found corresponding to the excel file TIN column and moved to the sorted folder ")
 
